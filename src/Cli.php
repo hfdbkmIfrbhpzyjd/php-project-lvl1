9fe919fd0cf7e9c92ab0e04725daf6a7d6c18345
Hfdbkm <hfdbkmIfrbhpzyjd@gmail.com>
2021-09-13 23:50:55 +0300

    5 step

--- src/Cli.php
+++ src/Cli.php
@@ -10,4 +10,14 @@ function greetings()
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
+}
+
+function result($res, $name)
+{
+    if ($res === 'win') {
+        line('Congratulations, %s!', $name);
+    } else if ($res === 'lose') {
+        line('Let\'s try again, %s!', $name);
+    }
 }
---
+++ src/brain-even-functions.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Brain\Games\EvenFunc;
+
+use function cli\line;
+use function cli\prompt;
+
+function even($var)
+{
+    return !($var & 1);
+}
+
+function roundEven()
+{
+    $num = rand(0, 100);
+    $expected = even($num) ? 'yes' : 'no';
+    line('Question: ' . (string) $num);
+    $answer = prompt('Your answer: ');
+    if ($expected === $answer) {
+        return 'win';
+    } else {
+        line("'%s' is wrong answer ;(. Correct answer was '%s'", $answer, $expected);
+        return 'lose';
+    }
+}
+
+function gameEven($numOfGames)
+{
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    for ($i = 0; $i < $numOfGames; $i++) {
+        $result = roundEven();
+        if ($result === 'win') {
+            line('Correct!');
+        } else if ($result === 'lose') {
+            return 'lose';
+        }
+    }
+    return 'win';
+}
